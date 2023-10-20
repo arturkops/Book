90cdbd943965ab5e042335c63dd2508dd706b1f5
arturkops <arturkops@yahoo.com>
2023-10-19 21:19:59 -0300

    agefix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from book import Book
 from book import Autor 
 
-autor_1 = Autor("Artur",18,"Felizardo",1997)
+autor_1 = Autor("Artur",26,"Felizardo",1997)
 autor_1.printData()
 print(autor_1.calc(2023))
